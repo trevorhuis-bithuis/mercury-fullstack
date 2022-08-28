cafb832b1294f59333422909205a2b0d87621de6
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2022-08-28 18:31:15 -0500

    Setup db for queries

--- client/src/routes/budget.jsx
+++ client/src/routes/budget.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import supabase from '../common/supabase';
 
 export default function Budget() {
+    useEffect(() => {
+        async function getTransactions() {
+            const { data, error } = await supabase
+                .from('transactions')
+                .select();
+
+            return data;
+        }
+        const transactions = getTransactions();
+
+        console.log(transactions);
+    }, []);
+
     return (
         <main style={{ padding: '1rem 0' }}>
             <div className='text-2xl'>Budgets</div>
